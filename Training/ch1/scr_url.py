63d71e5f065e8f4186c25bb78c8adeeeaa2b900c
harisuke <taiki6545@gmail.com>
2021-01-02 11:23:47 +0900

    #scr_urlはURLをとってくるはずだったもの失敗作

---
+++ Training/ch1/scr_url.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import urllib.request as req
+
+# 為替情報XMLを取得
+url = "https://news.yahoo.co.jp/rss/topics/it.xml"
+res = req.urlopen(url)
+#print(res,"res")
+print("*******************")
+# HTMLを解析
+soup = BeautifulSoup(res, "html.parser")
+#print(soup,"soup")
+print("*****************")
+# 任意のデータを抽出 --- (※1)
+#links = soup.find_all("item")
+links = soup.select("link")
+print("link=",links)
