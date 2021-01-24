598c8873046f7b23baa9fa758172de899e83942c
mofmofer <kawakami25ryota25@gmail.com>
2021-01-24 21:00:07 +0900

    add emcabtest.py fix 0124_tsuchioka.py

--- Training/ch1/0124_tsuchioka.py
+++ Training/ch1/0124_tsuchioka.py
@@ -1,4 +1,3 @@
-
 from bs4 import BeautifulSoup
 import urllib.request as req
 
@@ -8,9 +7,6 @@ response=req.urlopen(url)
 
 parse_html = BeautifulSoup(response,"html.parser")
 
-#print(parse_html.title.string)
-#print(parse_html.find_all("a"))
-
 list=parse_html.find_all("body")   #抜き出す箇所を指定
 
-print(title_list)
+print(list)
