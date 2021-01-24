0bacf64402852de9ebbb441fecc76ac61ac972ed
tsuchioka <shinzanmono.987@docomo.ne.jp>
2021-01-24 18:04:40 +0900

    URLからHTMLの中身をとってくるやつ（0124,土岡）

---
+++ Training/ch1/0124_tsuchioka.py
@@ -0,0 +1,17 @@
+pip install beautifulsoup4
+
+from bs4 import BeautifulSoup
+import urllib.request as req
+
+
+url="http://kino-code.work/python-scraping/"   #URLを指定
+response=req.urlopen(url)
+
+parse_html = BeautifulSoup(response,"html.parser")
+
+#print(parse_html.title.string)
+#print(parse_html.find_all("a"))
+
+list=parse_html.find_all("body")   #抜き出す箇所を指定
+
+print(title_list)
