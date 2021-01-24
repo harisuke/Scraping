c95320cd6371f09d2fafd8c492a83750d0ddff5d
harisuke <taiki6545@gmail.com>
2021-01-24 21:44:30 +0900

    #誰か助けて～

--- Training/ch1/word_to_url.py
+++ Training/ch1/word_to_url.py
@@ -6,16 +6,18 @@ url = 'https://www.google.co.jp/search'
 # グーグルへ接続
 req = requests.get(url, params={'q': 'パイソン'})
 
-
-
 html_list=req.text.split('"' )
+
 address_list=[]
 count=-1
+
 for i in html_list:
+    i=str(i)
     count+=1
-    postcode = re.match('http',i)
+    postcode = re.match('https',i)
     if postcode:
-        print(html_list[count])
+#        print(html_list[count])
         address_list.append(html_list[count])
 #    else:
 #        print ("失敗")
+print(address_list)
